b5c83d77b6ea470d8298d3bdd88e924243714754
sachinswillshall <73215941+sachinswillshall@users.noreply.github.com>
2021-12-02 17:51:01 +0530

    Add files via upload

---
+++ create-custom-shortcode.php
@@ -0,0 +1,15 @@
+<?php
+
+// function that runs when shortcode is called
+function wpb_demo_shortcode() { 
+ 
+// Things that you want to do. 
+$message = 'Hello world!'; 
+ 
+// Output needs to be return
+return $message;
+} 
+// register shortcode
+add_shortcode('greeting', 'wpb_demo_shortcode');
+
+?>
